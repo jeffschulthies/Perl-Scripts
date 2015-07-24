03e6d028516e71142cd892a503add94d35641d25
Jeff Schulthies <jeffschulthies@gwu.edu>
2015-07-24 19:14:57 -0400

    added crossproduct

---
+++ crossproduct.pl
@@ -0,0 +1,26 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+&crossproduct(@ARGV);
+
+sub crossproduct() {
+  my @tupleOne = split(/[,]/, &removeParens($_[0]));
+  my @tupleTwo = split(/[,]/, &removeParens($_[1]));
+
+  #print $tupleOne[1];
+  my @product;
+  $product[0] = $tupleOne[1] * $tupleTwo[2] - $tupleOne[2] * $tupleTwo[1];
+  $product[1] = $tupleOne[2] * $tupleTwo[0] - $tupleOne[0] * $tupleTwo[2];
+  $product[2] = $tupleOne[0] * $tupleTwo[1] - $tupleOne[1] * $tupleTwo[0];
+
+  foreach(@product) {
+    print "[$_]\n";
+  }
+
+}
+
+sub removeParens() {
+  $_[0] =~ s/[()]//g;
+  return $_[0];
+}
